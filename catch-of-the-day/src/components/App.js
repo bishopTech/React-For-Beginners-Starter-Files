b32c7cd9024095e210734e332b1a08aa2e54ef75
Chris Bishop <chris@bishop.tech>
2020-11-25 05:01:06 -0500

    completed lesson 13

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -6,13 +6,31 @@ import Order from './Order'
 
 
 class App extends React.Component {
+  state = {
+    fishes : {},
+    order : {}
+  }
+
+  addFish = (fish) => {
+    // how to update state : 
+    // First take a copy of state 
+    // add new thing to variable (add fish to fishes)
+    // set state with the new state variable (fishes)
+    const fishes = {...this.state.fishes }
+    fishes[`fish${Date.now()}`] = fish 
+    this.setState({
+      // oldFishes in current state: new fishes 
+      fishes
+    })
+  }
+  
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Chris is neat" />
           <Order />
-          <Inventory />
+          <Inventory addFish={this.addFish} />
         </div>
       </div>
     )
