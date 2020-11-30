7695e5354483542f26695b2e0c102fb54f7621ab
Chris Bishop <chris@bishop.tech>
2020-11-30 06:00:04 -0500

    completed lesson 19

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -13,7 +13,13 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    const { params } = this.props.match.params
+    const { params } = this.props.match;
+    // first reinstate localStorage
+    const localStorageRef = localStorage.getItem(params.storeId);
+    if (localStorageRef) {
+      this.setState({ order: JSON.parse(localStorageRef) });
+    }
+
     this.ref = base.syncState(`${params.storeId}/fishes`, {
       context: this,
       state: 'fishes'
@@ -22,6 +28,10 @@ class App extends React.Component {
 
   componentWillUnmount() {
     base.removeBinding(this.ref)
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem(this.props.match.params.storeId, JSON.stringify(this.state.order))
   }
 
   addFish = (fish) => {
--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -5,7 +5,9 @@ export default class Order extends Component {
     const fish = this.props.fishes[key];
     const count = this.props.order[key];
     const isAvailable = fish.status === "available";
-    
+    if (!fish) return null
+
+
     if (!isAvailable) {
       return (
         <li key={key}>
